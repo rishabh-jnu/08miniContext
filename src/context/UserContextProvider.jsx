df63098ebaeeaa8b94ddb16572b8f65c6a4a3964
Your Name <rishabhyadav70070@gmail.com>
2024-08-01 00:28:35 +0530

    Context Provider

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import UserContext from "./UserContext";
-import { Children } from "react";
 
-const UserContextProvider = () => {
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
     return(
-        <>
+        <UserContext.Provider value={{user, setUser}}>
         {children}
-        </>
+        </UserContext.Provider>
     )
 }
 
