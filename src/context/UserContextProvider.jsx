be88520bd7e6ee60795c2cbcaad726ea3bd8956a
Your Name <rishabhyadav70070@gmail.com>
2024-07-31 22:35:24 +0530

    Context API

---
+++ src/context/UserContextProvider.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserContext from "./UserContext";
+import { Children } from "react";
+
+const UserContextProvider = () => {
+    return(
+        <>
+        {Children}
+        </>
+    )
+}
+
+export default UserContextProvider
